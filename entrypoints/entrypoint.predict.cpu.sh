90c8fe2e2b7173c9410097a15cee6f8ba2fa08f0
Trung Ngo <codem01@gmail.com>
2022-10-15 06:19:35 -0500

    whitespace fixes and add predictions file param

--- entrypoints/entrypoint.predict.cpu.sh
+++ entrypoints/entrypoint.predict.cpu.sh
@@ -4,9 +4,11 @@ set -e
 BASE_MODEL_NAME=$1
 WEIGHTS_FILE=$2
 IMAGE_SOURCE=$3
+PREDICTIONS_FILE=$4
 
 # predict
 python -m evaluater.predict \
 --base-model-name $BASE_MODEL_NAME \
 --weights-file $WEIGHTS_FILE \
---image-source $IMAGE_SOURCE
+--image-source $IMAGE_SOURCE \
+--predictions-file $PREDICTIONS_FILE
